a587c2133b3953d57ee67581b73d25b676182b19
Игорь Чечет <ichechet@mail.ru>
2023-03-28 10:06:50 +0500

    Оформление

--- Examples/03 - Ticker.py
+++ Examples/03 - Ticker.py
@@ -9,15 +9,19 @@ if __name__ == '__main__':  # Точка входа при запуске это
 
     print('Получаем информацию обо всех тикерах (займет несколько секунд)...')
     securities = fp_provider.get_securities()  # Получаем информацию обо всех тикерах
+    # print('Ответ от сервера:', securities)
     for board, symbol in symbols:  # Пробегаемся по всем тикерам
-        si = next(item for item in securities.securities if item.board == board and item.code == symbol)
-        # print('Ответ от сервера:', si)
-        print(f'\nИнформация о тикере {si.board}.{si.code} ({si.short_name}, {fp_provider.markets[si.market]}):')
-        print(f'Валюта: {si.currency}')
-        decimals = si.decimals
-        print(f'Кол-во десятичных знаков: {decimals}')
-        print(f'Лот: {si.lot_size}')
-        min_step = 10 ** -decimals * si.min_step
-        print(f'Шаг цены: {min_step}')
+        try:
+            si = next(item for item in securities.securities if item.board == board and item.code == symbol)
+            # print(si)
+            print(f'\nИнформация о тикере {si.board}.{si.code} ({si.short_name}, {fp_provider.markets[si.market]}):')
+            print(f'Валюта: {si.currency}')
+            decimals = si.decimals
+            print(f'Кол-во десятичных знаков: {decimals}')
+            print(f'Лот: {si.lot_size}')
+            min_step = 10 ** -decimals * si.min_step
+            print(f'Шаг цены: {min_step}')
+        except:
+            print(f'\nТикер {board}.{symbol} не найден')
 
-    fp_provider.close_subscriptions_thread()  # Закрытие потока подписок перед выходом
+    fp_provider.close_subscriptions_thread()  # Закрываем поток подписок перед выходом
